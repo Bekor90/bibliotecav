9037652cbe243c2f8101431a9b1034fbd984f0de
John Vanegas <jhon.anderson.vanegas@gmail.com>
2017-09-10 08:48:44 -0500

    change

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url', 'Cargarconsultas_helper');
+$autoload['helper'] = array();
 
 /*
 | -------------------------------------------------------------------
