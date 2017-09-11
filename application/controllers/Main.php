efd4e157ebaeb8e7a333c7f96aa7d10b16de1640
John Vanegas <jhon.anderson.vanegas@gmail.com>
2017-09-11 10:47:25 -0500

    test form validation

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -724,7 +724,7 @@ class Main extends CI_Controller
 		 $this->load->library('form_validation');
 		$area = $this->input->post('nombreArea', TRUE);
 		    	// reglas de validacion
-	    $this->form_validation->set_rules('nombreArea', 'trim|required||strip_tags|min_length[2]|maxlength[50]');
+	    $this->form_validation->set_rules('nombreArea', 'trim|required|strip_tags|min_length[2]|maxlength[50]');
 	    $this->form_validation->set_message('required', 'Debe completar este campo');
 	    $this->form_validation->set_message('min_length[3]','El campo debe tener mas de 3 caracteres');
 	    $this->form_validation->set_message('maxlength[50]','El campo debe tener menos de 50 caracteres');
@@ -736,7 +736,6 @@ class Main extends CI_Controller
 	    }
 	    else 
 	    {	
-
 	    	 echo $area;
 	    	
 		// $this->Tbl_areas->guardarArea($area);
