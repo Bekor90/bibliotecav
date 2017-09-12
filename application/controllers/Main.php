3ed8f834d2a41e47444c5ced57c2487677dff51f
John Vanegas <jhon.anderson.vanegas@gmail.com>
2017-09-12 07:45:24 -0500

    test load helper

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -6,7 +6,7 @@ class Main extends CI_Controller
 	 public function __construct()
         {
             parent::__construct();        
-            //$this->load->helper(array('url'));
+            $this->load->helper(array('url', 'Cargarconsultas'));
             $this->load->model('Tbl_documentos');
             $this->load->model('Tbl_areas');
             $this->load->model('Tbl_palabras');
