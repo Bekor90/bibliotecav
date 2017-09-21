655c5065bcc41b2177c5b3ecad0ec28296bcd722
John Vanegas <jhon.anderson.vanegas@gmail.com>
2017-09-21 03:21:26 -0500

    test succes

--- application/views/headers/menutemplate.php
+++ application/views/headers/menutemplate.php
@@ -90,7 +90,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <h1>
-        <?php echo  $views;?>
+        <?php echo  $titulo;?>
       </h1>
       <ol class="breadcrumb">
         <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
